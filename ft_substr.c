de4a892a8162f8c937ea64ba6e42de6d1bd2b895
Roberto Arce <roberto_arce_@hotmail.com>
2020-05-15 10:03:32 +0200

    fixing the len 0 in substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rarce <rarce@42.student.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 12:46:53 by rarce             #+#    #+#             */
-/*   Updated: 2020/05/13 16:47:34 by rarce            ###   ########.fr       */
+/*   Updated: 2020/05/15 10:02:44 by rarce            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*ptr;
 	unsigned int	counter;
 
-	if (!s || start > (unsigned int)ft_strlen(s)
-			|| start + len > (unsigned int)ft_strlen(s))
+	if (!s)
 		return (NULL);
+	if (start > (unsigned int)ft_strlen(s))
+		len = 0;
 	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	counter = 0;
