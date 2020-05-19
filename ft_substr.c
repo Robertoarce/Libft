3f09442332b08bd0ba846b49501b616a73552885
Robertoarce <roberto_arce_@hotmail.com>
2020-05-19 18:45:26 +0200

    Update ft_substr.c
    avoiding possible seg fault

--- ft_substr.c
+++ ft_substr.c
@@ -24,7 +24,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	counter = 0;
-	while (len > 0)
+	while (len > 0 && s[start + counter])
 	{
 		ptr[counter] = s[start + counter];
 		counter++;
