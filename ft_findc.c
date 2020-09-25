72eb7159cd6d16bac54a5d5251d2f0d288da63c3
Roberto arce <roberto_arce_@hotmail.com>
2020-09-25 19:46:32 +0200

    protecting against null value

--- ft_findc.c
+++ ft_findc.c
@@ -6,7 +6,7 @@
 /*   By: titorium <rarce@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/27 11:48:58 by titorium          #+#    #+#             */
-/*   Updated: 2020/07/27 20:45:05 by titorium         ###   ########.fr       */
+/*   Updated: 2020/09/25 19:44:30 by titorium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	ft_findc(const char *tab, char c)
 {
 	int counter;
 
+	if (tab == NULL)
+		return (-1);
 	counter = 0;
-	while (tab[counter] && tab[counter + 1] && tab)
+	while (tab[counter] != '\0')
 	{
 		if (tab[counter] == c)
 			return (counter);
